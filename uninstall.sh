f5b446cbca1d8c62ef4983773f038462774a5874
Erick Levandoski <157233531+Erick-IL@users.noreply.github.com>
2025-06-03 16:54:33 -0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,10 +2,10 @@
 
 sudo apt remove --purge -y polybar rofi
 
-rm -rf ~/.config/polybar
+sudo  rm -rf ~/.config/polybar
 
-rm -f ~/.config/autostart/polybar.desktop
+sudo rm -f ~/.config/autostart/polybar.desktop
 
-killall grep polybar
+sudo killall grep polybar
 
 echo "Rice desinstalado com sucesso!"
